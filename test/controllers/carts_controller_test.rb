27e3b655d86fb8b4209069bc9f438f5aea030838
Ahmed O <aomra015@gmail.com>
2013-07-18 21:01:51 +0000

    make carts controller tests pass

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class CartsControllerTest < ActionController::TestCase
   test "should get show" do
-    skip
-    get :show
+    get :show, id: carts(:one)
     assert_response :success
   end
 
