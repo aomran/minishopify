453edd42622a0331fcaa81fe8fb16434145babfb
Ahmed O <aomra015@gmail.com>
2013-07-22 19:38:17 +0000

    implement line item increments

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,16 @@ class Cart < ActiveRecord::Base
 	has_many :line_items
 	
 	def add_product(product_id)
-		line_items.build(product_id: product_id)
+		
+		current_item = line_items.find_by(product_id: product_id)
+
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(product_id: product_id)
+		end
+		current_item
+		
 	end
 
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
   
-  test "adds line items to cart" do
-  	@cart = Cart.create
-  	assert_instance_of(LineItem, @cart.add_product(1))
+  test "should add line item to cart if it doesn't exist" do
+  	line_item = Cart.create.add_product(products(:one))
+  	assert_instance_of LineItem, line_item
+  	assert_equal line_item.quantity, 1
   end
+
+  test "should increment line item if it exists" do
+  	incremented_item = carts(:one).add_product(products(:one))
+  	assert_instance_of(LineItem, incremented_item)
+  	assert_equal(2, incremented_item.quantity)
+  end
+
 end
