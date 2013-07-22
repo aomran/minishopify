058a5b68c608ca84c2328817bf54d095760995ae
Ahmed O <aomra015@gmail.com>
2013-07-22 20:03:36 +0000

    refactor line items controller

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,18 +1,20 @@
 class LineItemsController < ApplicationController
+  before_action :set_cart
 
   def create
-    @product = Product.find_by_id(params[:product])
-
-    if session[:cart_id] == nil
-    	@cart = Cart.create
-    	session[:cart_id] = @cart.id
-    else
-    	@cart = Cart.find_by_id(session[:cart_id])
-    end
-
+    @product = Product.find(params[:product])
     @line_item = @cart.add_product(@product.id)
     @line_item.save
 
     redirect_to cart_path(@cart)
+  end
+
+  def set_cart
+    if session[:cart_id].nil?
+        @cart = Cart.create
+        session[:cart_id] = @cart.id
+    else
+        @cart = Cart.find(session[:cart_id])
+    end
   end
 end
