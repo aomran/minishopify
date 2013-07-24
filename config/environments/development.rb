5dd7dc8ea7a178a86b0761a25a52f93556a59e72
Ahmed O <aomra015@gmail.com>
2013-07-24 14:45:24 -0400

    add email ability in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,15 @@ Minishopify::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+    enable_starttls_auto: true
+  }
 end
